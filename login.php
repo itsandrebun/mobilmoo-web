64a5069fa4867707c65727148cbc56eea53ae0aa
Andre Bun <andreebun97@gmail.com>
2020-12-15 16:42:43 +0700

    Pengkategorian footer dan header

--- login.php
+++ login.php
@@ -3,28 +3,32 @@
 ?>
 <?php include 'templates/header.php';?>
   <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 auth-form">
-                <div class="mt-3 mb-4">
-                    <h2 class="auth-title" style="color:#086dff;cursor:pointer" onclick="window.location.href = 'index.php'">mobilmoo</h2>
-                </div>
-                <div class="mb-5">
-                    <span>Hello! Nice to meet you.</span>
-                    <h1 class="auth-title">Sign Up to Mobilmoo</h1>
-                    <span class="redirect-to-login">Don't have an account? <a href="register.php">Register</a></span>
-                </div>
-                <form>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Email/Phone Number</label>
-                        <input type="text" class="form-control" name="username" placeholder="example: mobilmoo">
+                <div>
+                    <div class="mt-3">
+                        <h2 class="auth-title" style="color:#086dff;cursor:pointer;text-align:left" onclick="window.location.href='index.php'">mobilmoo</h2>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Password</label>
-                        <input type="password" class="form-control" name="password" placeholder="6+ Characters, 1 Capital Letter">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-lg w-100 p-3 mt-3" style="text-align:left;font-weight:bold">Sign In <i style="float:right" class="fa fa-arrow-right mt-1"></i></button>
-                </form>
+                    <form class="d-flex align-items-center justify-content-start h-100" style="min-height:90vh">
+                        <div>
+                            <div class="mb-5">
+                                <span>Hello! Nice to meet you.</span>
+                                <h1 class="auth-title">Sign Up to Mobilmoo</h1>
+                                <span class="redirect-to-login">Don't have an account? <a href="register.php">Register</a></span>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Email/Phone Number</label>
+                                <input type="text" class="form-control" name="username" placeholder="example: mobilmoo">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-label">Password</label>
+                                <input type="password" class="form-control" name="password" placeholder="6+ Characters, 1 Capital Letter">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-lg w-100 p-3 mt-3" style="text-align:left;font-weight:bold">Sign In <i style="float:right" class="fa fa-arrow-right mt-1"></i></button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div class="col-md-6 mobilmoo-introduction">
                 <h1 class="pt-5">mobilmoo</h1>
@@ -34,15 +38,6 @@
         </div>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
+    <?php include "templates/footer.php";?>
   </body>
 </html>
